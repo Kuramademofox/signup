a4c83a005c9ea45b1c5583b84ab206c1c79fe21b
Kurama <166109539+Kuramademofox@users.noreply.github.com>
2024-10-16 12:44:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCQZVjMXCZ08lbOowSJrdqKx8kCZtXnmu0",
-  authDomain: "sign-in-4fe48.firebaseapp.com",
-  projectId: "sign-in-4fe48",
-  storageBucket: "sign-in-4fe48.appspot.com",
-  messagingSenderId: "209201210883",
-  appId: "1:209201210883:web:4bfe1ea93c0d9426565036",
-  measurementId: "G-KK3Y6Z7PG1"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCQZVjMXCZ08lbOowSJrdqKx8kCZtXnmu0",
+    authDomain: "sign-in-4fe48.firebaseapp.com",
+    projectId: "sign-in-4fe48",
+    storageBucket: "sign-in-4fe48.appspot.com",
+    messagingSenderId: "209201210883",
+    appId: "1:209201210883:web:4bfe1ea93c0d9426565036",
+    measurementId: "G-KK3Y6Z7PG1"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 document.addEventListener('DOMContentLoaded', () => {
     const sendOTPButton = document.getElementById('sendOTP');
